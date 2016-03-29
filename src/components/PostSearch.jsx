fa782fb3ef0e92139773a9cc9633ac5588851c5c
Claudio Savino <claudio.savino@cloudmesh.pro>
2016-03-29 02:32:48 +0200

    update

--- src/components/PostSearch.jsx
+++ src/components/PostSearch.jsx
@@ -8,21 +8,38 @@ import { connect } from '../state/context';
 @connect
 export default class PostSearch extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { search: '' };
+  }
+
   handleSearch = (e) => {
     e.preventDefault();
     const val = e.target.value;
+    this.setState({ search: val });
     this.context.store.post.search(val);
+  };
+
+  resetSearch = (e) => {
+    e.preventDefault();
+    this.setState({ search: '' });
+    this.context.store.post.search('');
   };
 
   render() {
     return (
       <form>
         <input
-          className="field"
+          className="field rounded-left"
           type="text"
           placeholder="Search..."
+          value={this.state.search}
           onChange={this.handleSearch}
         />
+        <button
+          onClick={this.resetSearch}
+          className="btn rounded-right border black bg-silver"
+        >X</button>
       </form>
     );
   }
