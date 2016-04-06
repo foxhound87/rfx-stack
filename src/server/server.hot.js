74b56b63139bd60cc369e25364fe0b249e2f7b77
Claudio Savino <savino87@gmail.com>
2016-04-06 21:15:42 +0200

    update

--- src/server/server.hot.js
+++ src/server/server.hot.js
@@ -1,20 +1,17 @@
 import feathers from 'feathers';
 import compression from 'compression';
-import serveStatic from 'serve-static';
 
+import { serveStaticMiddleware } from './middleware/serveStatic';
 import { hotMiddleware } from './middleware/hot';
 import { isoMiddleware } from './middleware/iso';
 import { startWebServer as start } from './start';
-
-import { Dir } from '../config';
 
 const app = feathers();
 
 app
   .use(compression())
   .set('view engine', 'ejs')
-  .use('/build', serveStatic(Dir.build))
-  .use('/static', serveStatic(Dir.static))
+  .configure(serveStaticMiddleware)
   .use(hotMiddleware)
   .use(isoMiddleware)
   .configure(start);
