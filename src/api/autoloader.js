2eb4f475e5275b015816a9a01d5cbc46966ead4d
Claudio Savino <claudio.savino@me.com>
2016-10-17 18:56:45 +0200

    fix require services autoload

--- src/api/autoloader.js
+++ src/api/autoloader.js
@@ -5,8 +5,8 @@ const Dir = global.DIR;
 
 export function autoloader($service) {
   const dir = $service.replace(Dir.api, '.'); // require build fix (".replace()")
-  const ServiceConfig = require([dir, 'config.js'].join('/')).default;
-  const ServiceModel = require([dir, 'model.js'].join('/')).default;
+  const ServiceConfig = require(dir + '/config.js').default; // eslint-disable-line
+  const ServiceModel = require(dir + '/model.js').default; // eslint-disable-line
 
   // extend the service object with related model
   Object.assign(ServiceConfig.options, { Model: ServiceModel });
@@ -22,8 +22,6 @@ export function autoloader($service) {
   const service = this.app.service(ServiceConfig.namespace);
 
   // Setup our HOOKS (before/after)
-  const beforeHooksDir = [dir, 'hooks.before.js'].join('/');
-  const afterHooksDir = [dir, 'hooks.after.js'].join('/');
-  service.before(require(beforeHooksDir).default);
-  service.after(require(afterHooksDir).default);
+  service.before(require(dir + '/hooks.before.js').default); // eslint-disable-line
+  service.after(require(dir + '/hooks.after.js').default); // eslint-disable-line
 }
