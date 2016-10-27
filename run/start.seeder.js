b71cdeeade5d6c1df93ec654112ce04e0904c598
Claudio Savino <claudio.savino@me.com>
2016-10-28 01:38:19 +0200

    #30

--- run/start.seeder.js
+++ run/start.seeder.js
@@ -1,3 +1,10 @@
+require('babel-core/register');
 require('./init');
+
+const getenv = require('getenv');
+const env = require('../config/expose');
+
+global.CONFIG = getenv.multi(env).default;
+
 require('../src/utils/seeder.runner')
   .default('./src/seeds/');
